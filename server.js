c938b0b819ac438a91726cabd0684e9c8f66ac97
Mark-A-A <markawai01@gmail.com>
2016-02-20 13:47:54 -0500

    handlebars in progress

--- server.js
+++
@@ -1,7 +0,0 @@
-var express           = require('express');
-var expressHandlebars = require('express-handlebars');
-var bodyParser       = require('body-parser');
-var session           = require('express-session');
-var Sequelize         = require('sequelize');
-var mysql             =require('mysql');
-var app               = express();
